37e0cc9595909aba6ff2d1a52b33bd08685d8327
piopratama <piopratama2@gmail.com>
2022-07-12 13:51:14 +0800

    1. create splash screen (design only) 2. create login screen (design only)

---
+++ app/src/main/java/com/example/temanagen/SplashScreen.kt
@@ -0,0 +1,32 @@
+package com.example.temanagen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 1000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed(
+            {
+                val i = Intent(this, LoginScreen::class.java)
+                startActivity(i)
+                finish()
+            }, SPLASH_TIME_OUT)
+    }
+}
